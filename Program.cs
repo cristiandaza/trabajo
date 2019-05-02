b8da21b6965afa8a4eeb45a0580e6951e4c6a499
Gambitodg <dagamboa22@misena.edu.co>
2019-05-02 14:59:50 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int edad;
+            Console.WriteLine("Bienvenido a C#");
+            Console.WriteLine("Ingrese su Nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad");
+            edad = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Bienvenido al Sitema: {nombre}  Tu edad es: {edad}");
+
+            Console.ReadLine();
+        }
+    }
+}
