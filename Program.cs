3abdc3fd0c88825b4c57c131040defe1a608be3d
Gambitodg <dagamboa22@misena.edu.co>
2019-05-02 16:34:22 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,19 +10,34 @@ namespace AppWeb
     {
         static void Main(string[] args)
         {
-            string nombre, apellido;
-            int edad;
-            Console.WriteLine("Bienvenido a C#");
-            Console.WriteLine("Ingrese su Nombre");
-            nombre = Console.ReadLine();
-            Console.WriteLine("Ingrese su Apellido");
-            apellido = Console.ReadLine();
-            Console.WriteLine("Ingrese su edad");
-            edad = int.Parse(Console.ReadLine());
-    
-            Console.WriteLine($"Bienvenido al Sitema: {nombre} {apellido}  Tu edad es: {edad}");
-            
+            int sum = 0;
+            int  sum2 = 0;
+            for (int i = 0; i < 5; i++)
+            {
 
+
+                string nombre, apellido;
+                int edad;
+                Console.WriteLine("Bienvenido a C#");
+                Console.WriteLine("Ingrese su Nombre");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese su Apellido");
+                apellido = Console.ReadLine();
+                Console.WriteLine("Ingrese su edad");
+                edad = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Bienvenido: {nombre} {apellido}  Tu edad es: {edad}");
+                if (edad >= 18)
+                {
+                    Console.WriteLine("Puedes ingresar a la fiesta");
+                    sum = sum + 1;
+                }
+                else
+                {
+                    Console.WriteLine("No se te permite ingresar a la fiesta eres menor de edad");
+                    sum2 = sum2 + 1;
+                }
+            }
+            Console.WriteLine($"El número de personas mayores de edad es: {sum}  El número de personas menores de edad es:  {sum2}");
             Console.ReadLine();
         }
     }
